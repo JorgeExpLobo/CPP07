e46f7026f762f0dbc73aabaececd0a3c6e32786b
Jorge Expósito <jolfuw@MacBook-Pro-de-Jorge.local>
2026-04-29 13:17:46 +0200

    Added testing for const types

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include <cstddef>
 
 template <typename T_ARRAY, typename T_FUNC>
-void iter(T_ARRAY *array, const size_t length, T_FUNC func) {
-    for (size_t i = 0; i < length; i++) {
+void iter(T_ARRAY *array, const std::size_t length, T_FUNC func) {
+    for (std::size_t i = 0; i < length; i++) {
         func(array[i]);
     }
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "iter.hpp"
+#include <string>
 #include <iostream>
 
 void charToLower(char &c) {
@@ -7,10 +8,15 @@ void charToLower(char &c) {
 }
 
 void stringToLower(std::string &str) {
-    for (size_t i = 0; i < str.length(); i++) {
+    for (std::size_t i = 0; i < str.length(); i++) {
         if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] += 32;
     }
+}
+
+template <typename T>
+void printElement(const T& element) {
+    std::cout << element << std::endl;
 }
 
 void timesFive(int &num) {
@@ -24,7 +30,7 @@ int main(void) {
     for (int i = 0; i < 4; i++)
 		std::cout << int_array[i] << std::endl;
     ::iter(int_array, 4, timesFive);
-    std::cout << "--- UPDATED STRINGS ---" << std::endl;
+    std::cout << "--- UPDATED INTEGERS ---" << std::endl;
 	for (int i = 0; i < 4; i++)
 		std::cout << int_array[i] << std::endl;
 
@@ -36,5 +42,10 @@ int main(void) {
     std::cout << "--- UPDATED STRINGS ---" << std::endl;
     for (int i = 0; i < 4; i++)
 		std::cout << str_array[i] << std::endl;
+
+    const int const_array[] = {1, 120, 25, 3};
+    std::cout << "--- CONST INTEGERS ---" << std::endl;
+    ::iter(const_array, 4, printElement<int>);
+
     return (0);
 }
