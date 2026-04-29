7e6757f7449321eb9656f64a5415f6c2f468ad59
Jorge Expósito <jolfuw@MacBook-Pro-de-Jorge.local>
2026-04-29 12:10:30 +0200

    Fixed main.cpp to be the one on the subject on ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,38 +1,21 @@
 #include "whatever.hpp"
 #include <iostream>
 
-int main() {
-    std::cout << "--- INTEGERS ---" << std::endl;
-    int a = 4;
-    int b = 7;
-    int *a_ptr = &a;
-    int *b_ptr = &b;
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
+int main(void) {
+    int a = 2;
+    int b = 3;
 
-    std::cout << "min = " << min(a, b) << std::endl;
-    std::cout << "max = " << max(a, b) << std::endl;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    swap(*a_ptr, *b_ptr);
-    std::cout << "          --SWAPPING INTEGERS--" << std::endl;
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
 
-    std::cout << "--- CHARS ---" << std::endl;
-    char c = 'c';
-    char d = 'd';
-    char *c_ptr = &c;
-    char *d_ptr = &d;
-    std::cout << "c = " << c << std::endl;
-    std::cout << "d = " << d << std::endl;
-
-    std::cout << "min = " << min(c, d) << std::endl;
-    std::cout << "max = " << max(c, d) << std::endl;
-
-    swap(*c_ptr, *d_ptr);
-    std::cout << "          --SWAPPING CHARS--" << std::endl;
-    std::cout << "c = " << c << std::endl;
-    std::cout << "d = " << d << std::endl;
-
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     return 0;
 } 
